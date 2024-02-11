f8980867a7c556ff520e378a898f0371c1197bd1
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-02-10 20:24:35 -0800

    Declared field tasks in server

--- register_server.cc
+++ register_server.cc
@@ -8,24 +8,23 @@ void RegisterServiceImpl::BuildRegisterArrayResponse(::register_service::Respons
     if (it == _registers.end()) {
         return;
     }
-    // Task: Copy the register array `it` to response (follow the following five instructions)
+    //Copy the register array `it` to response 
     
-    // 1. Get the register array from `it`
-    // RegisterArray* curr_reg = ...
+    // Get the register array from `it`
+    RegisterArray* curr_reg = it->second;
 
-    // 2. Add a new register array to `response`
-    // RegisterArray* new_reg = response->...
+    // Add a new register array to `response`
+    RegisterArray* new_reg = response->mutable_arrays()->add_arrays();
     new_reg->set_name(curr_reg->name());
     new_reg->set_capacity(curr_reg->capacity());
     new_reg->set_size(curr_reg->size());
 
-    // 3. Copy items from `curr_reg` to `new_reg`
-    // ::google::protobuf::RepeatedField<uint32_t>* items = ..
-    ::google::protobuf::RepeatedField<uint32_t>::iterator curr_it;
+    //Copy items from `curr_reg` to `new_reg`
+    const ::google::protobuf::RepeatedField<uint32_t>& items = curr_reg->items();
     uint32_t item_idx = 0;
-    for (/*4. complete iteration*/) {
-        // 5. Add an item
-        // new_reg->...
+    for (::google::protobuf::RepeatedField<uint32_t>::const_iterator curr_it = items.begin(); curr_it != items.end(); curr_it++) {
+        //Add an item
+        new_reg->add_items(*curr_it);
     }
 }
 
@@ -33,10 +32,11 @@ void RegisterServiceImpl::BuildRegisterArrayResponse(::register_service::Respons
 RegisterServiceImpl::GetAllRegisterArrays(::grpc::ServerContext* context,
                                           const ::google::protobuf::Empty* request,
                                           ::register_service::Response* response) {
-    std::map<std::string, RegisterArray*>::iterator it;
-    // Task: output all register arrays to `response`
-    // Iterate over the register arrays in `_registers`, and call `BuildRegisterArrayResponse`
-    
+    // Output all register arrays to `response`
+    for (std::map<std::string, RegisterArray*>::iterator it = _registers.begin(); it != _registers.end(); it++) {
+        // Call BuildRegisterArrayResponse to populate the response with the register array
+        BuildRegisterArrayResponse(response, it);
+    }
     return Status::OK;
 }
 
@@ -47,16 +47,16 @@ RegisterServiceImpl::GetRegisterArray(::grpc::ServerContext* context,
     std::string name = request->name();
     // Task: output the requested register array to `response`
     // 1. Search for the register array in `_registers`
+    std::map<std::string, RegisterArray*>::iterator it = _registers.find(name);
     // 2. If it doesn't exist, the body of the `if` statement should execute
-    // 3. Otherwise, call BuildRegisterArrayResponse
-
-    // std::map<std::string, RegisterArray*>::iterator it = ...
-    if (/*complete condition*/) {
+    if (it == _registers.end())
+    {
         std::string msg("[GET] Register " + name + " doesn't exist");
         return Status(StatusCode::NOT_FOUND, msg);
     }
 
-    // Call BuildRegisterArrayResponse
+    // 3. Otherwise, call BuildRegisterArrayResponse
+    BuildRegisterArrayResponse(response, it);
 
     return Status::OK;
 }
@@ -68,30 +68,29 @@ RegisterServiceImpl::CreateRegisterArray(::grpc::ServerContext* context,
     std::string name = request->name();
     uint32_t capacity = request->capacity();
 
-    // Task: create a new register array
-    // 1. Search for the register array in `_registers`
-    // 2. If it exists, the body of the `if` statement should execute
-    // 3. Otherwise, create a new RegisterArray object and add it to `_registers`
-    // All new items should be zero'd
+    // Create a new register array
+    // Search for the register array in `_registers`
+    std::map<std::string, RegisterArray*>::iterator it = _registers.find(name);
     
-    // std::map<std::string, RegisterArray*>::iterator it = ...
-    if (/*complete condition*/) {
+    // 2. If it exists, the body of the `if` statement should execute
+    if (it != _registers.end()) 
+    {
         std::string msg("[CREATE] Register: " + name + " already exists");
         return Status(StatusCode::ALREADY_EXISTS, msg);
     }
-
-    // Complete
-    // RegisterArray* r = ...
-    // r->set_XXX; // set the name
-    // r->set_XXX; // set the size
-    // r->set_XXX; // set the capacity
+    // 3. Otherwise, create a new RegisterArray object and add it to `_registers`
+    // All new items should be zero'd
+    RegisterArray* r = new RegisterArray();
+    r->set_name(name); // set the name
+    r->set_size(0); // set the size
+    r->set_capacity(capacity); // set the capacity
     // All items are initialized with zeros
     for (int i = 0; i < capacity; i++) {
         r->mutable_items()->Add(0);
     }
 
     // Add the register array to `_registers`
-    //_registers.insert(...);
+    _registers.insert(std::make_pair(name, r));
     _written_indices.insert(std::pair<std::string, std::set<uint32_t>*>(name, new std::set<uint32_t>()));
     return Status::OK;
 }
@@ -102,19 +101,20 @@ RegisterServiceImpl::DeleteRegisterArray(::grpc::ServerContext* context,
                                          ::register_service::Response* response) {
     std::string name = request->name();
 
-    // Task: delete a register array
+    // delete a register array
     // 1. Search for the register array in `_registers`
+    std::map<std::string, RegisterArray*>::iterator it = _registers.find(name);
+
     // 2. If it doesn't exist, the body of the `if` statement should execute
-    // 3. Otherwise, remove the register array from `_registers` and `_written_indices`
-    
-    // std::map<std::string, RegisterArray*>::iterator it = ...
-    if (/*complete condition*/) {
+    if (it == _registers.end()) {
         std::string msg("[DELETE] Register: " + name + " doesn't exist");
         return Status(StatusCode::NOT_FOUND, msg);
     }
-    
+
+    // 3. Otherwise, remove the register array from `_registers` and `_written_indices`
     // Remove the register array from `_registers`
-    //_registers.erase(...);
+    delete it->second; // Free memory for the register array
+    _registers.erase(it);
     _written_indices.erase(name);
 
     return Status::OK;
@@ -129,25 +129,25 @@ RegisterServiceImpl::ReadValue(::grpc::ServerContext* context,
 
     // Task: read a value from a register array
     // 1. Search for the register array in `_registers`
+    std::map<std::string, RegisterArray*>::iterator it = _registers.find(name);
     // 2. If it doesn't exist, the body of the first `if` statement should execute
-    // 2. If it exists:
-    // 2.a) if `index` is larger than register's capacity, the body of the second `if` statement should execute
-    // 2.b) Otherwise, read the value to `response`
-    
-    // std::map<std::string, RegisterArray*>::iterator it = ...
-    if (/*complete condition*/) {
+    if (it == _registers.end()) {
         std::string msg("[READ] Register: " + name + " doesn't exist");
         return Status(StatusCode::NOT_FOUND, msg);
     }
-
-    // RegisterArray* r = ...
-    if (/*complete condition*/) {
+    
+    // 2. If it exists:
+    // 2.a) if `index` is larger than register's capacity, the body of the second `if` statement should execute
+    // 2.b) Otherwise, read the value to `response`
+    RegisterArray* r = it->second;
+    if (index >= r->capacity()) 
+    {
         std::string msg("[READ] Register: " + name + ", Index: " + std::to_string(index) + " invalid");
         return Status(StatusCode::OUT_OF_RANGE, msg);
     }
 
     // get the value here
-    // uint32_t value = ...
+    uint32_t value = r->items(index);
     response->set_value(value);
     return Status::OK;
 }
@@ -162,29 +162,31 @@ RegisterServiceImpl::WriteValue(::grpc::ServerContext* context,
 
     // Task: write a value to a register array
     // 1. Search for the register array in `_registers`
-    // 2. If it doesn't exist, the body of the first `if` statement should execute
-    // 2. If it exists:
-    // 2.a) if `index` is larger than register's capacity, the body of the second `if` statement should execute
-    // 2.b) Otherwise, modify the item using the given `index`
+    std::map<std::string, RegisterArray*>::iterator it_reg = _registers.find(name);
+    std::map<std::string, std::set<uint32_t>*>::iterator it_wrt = _written_indices.find(name);
 
-    // std::map<std::string, RegisterArray*>::iterator it_reg = ...
-    // std::map<std::string, std::set<uint32_t>*>::iterator it_wrt = ...
-    if (/*complete condition*/) {
+    // 2. If it doesn't exist, the body of the first `if` statement should execute
+    if (it_reg == _registers.end()) {
         std::string msg("[WRITE] Register: " + name + " doesn't exist");
         return Status(StatusCode::NOT_FOUND, msg);
     }
 
-    // RegisterArray* reg = ...
+    // 2. If it exists:
+    // 2.a) if `index` is larger than register's capacity, the body of the second `if` statement should execute
+    // 2.b) Otherwise, modify the item using the given `index`
+    
+    RegisterArray* reg = it_reg->second;
     uint32_t capacity = reg->capacity();
     uint32_t size = reg->size();
 
-    if (/*complete condition*/) {
+    if (index >= capacity) {
         std::string msg("[WRITE] Register: " + name + ", Index: " + std::to_string(index) + " invalid");
         return Status(StatusCode::OUT_OF_RANGE, msg);
     }
 
     // Modify the item here
-    // ...
+    reg->set_items(index, value);
+    reg->set_size(size + 1);
     
     // Keep these lines
     std::set<uint32_t>* written = it_wrt->second;
