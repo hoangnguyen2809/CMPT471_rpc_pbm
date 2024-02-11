691b599bde6fd699d77dd30b2e24505d009a3e4f
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-02-10 21:16:03 -0800

    Test case implemented

--- rpc_client.cc
+++ rpc_client.cc
@@ -19,20 +19,29 @@ int main(int argc, char** argv) {
     client.ReadValue("test", 0);
     client.ReadValue("test", 1);
     client.ReadValue("test1", 1);
+    client.ReadValue("test1", 55);
+
+    Result* r1 = client.GetRegisterArray("test");
 
     client.WriteValue("test", 0, 100);
     client.WriteValue("test", 0, 200);
     client.WriteValue("test", 1, 2);
     client.WriteValue("test", 1, 3);
-    client.WriteValue("test1", 1, 3);
+    client.WriteValue("test1", 0, 3);
+    client.WriteValue("test1", 1, 333);
+    client.WriteValue("test1", 2, 355);
+    client.WriteValue("test1", 18, 366);
+    client.WriteValue("test1", 32, 388);
     client.WriteValue("test", 100, 3);
 
     client.ReadValue("test", 0);
     client.ReadValue("test", 1);
     client.GetAllRegisterArrays();
-    Result* r = client.GetRegisterArray("test1");
+    Result* r2 = client.GetRegisterArray("test1");
     client.DeleteRegisterArray("test1");
     client.GetRegisterArray("test1");
+    client.GetAllRegisterArrays();
+    client.DeleteRegisterArray("test");
 
     return 0;
 }
